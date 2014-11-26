caeca49cd2aa423257fd24b8a231efed51070717
Denis Zhbankov <denis.zhbankov@gmail.com>
2014-11-27 01:17:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# node-dadata
+Tiny NPM module for the REST API of dadata.ru
+
+
+## usage example
+```javascript
+var dadata = require('dadata')(process.env.DADATA_API_KEY, process.env.DADATA_SECRET_KEY);
+dadata('address', ['мск сухонска 11/-89'], function (err, res) {
+  console.log(err);
+  console.log(res);
+});
+```
+
+
+## formal docs
+
+Before use, this service must be properly configured: you have to provide your `API_KEY` and `SECRET_KEY` to the constructor.
+```javascript
+  var Dadata = require('dadata'),
+      dadata_account_1 = new Dadata('<API_KEY_1>', '<SECRET_KEY_1>'),
+      dadata_account_2 = new Dadata('<API_KEY_2>', '<SECRET_KEY_2>');
+```
+
+If you don't plan to utilize multiple instances of dadata service, it's also possible to use the short notation.
+```javascript
+  var dadata = require('dadata')('<YOUR_API_KEY>', '<YOUR_SECRET_KEY>');
+```
+
+Exposed `dadata` service is the function of 3 arguments (`function (apiType, queries, callback) {...}`), where:
+  + api type -- one of `['address', 'birthdate', 'email', 'name', 'phone']`;
+  + queries array -- array of one or more strings;
+  + callback -- `function (error, result) {...}`; if everything's fine, then `error` is undefined and `result` contains a dadata response (an array of objects, corresponding to your array of queries); if something went wrong, `error` will contain an object with `code` and `message` fields, and `result` will be undefined.
---
+++ index.js
@@ -0,0 +1,91 @@
+'use strict';
+var https = require('https'),
+    apiUrl = '/api/v2/clean/',
+    apiTypes = ['address', 'birthdate', 'email', 'name', 'phone'];
+
+// request sender
+function reqSend (apiType, queries, callback) {
+  /* jshint validthis:true */
+  var req;
+  
+  // handle invalid apiType
+  if (apiTypes.indexOf(apiType) === -1)
+    return callback({
+      code: -2,
+      message: 'node-dadata: invalid apiType'
+    });
+
+  // ok
+  else {
+
+    // set api endpoint
+    this.reqOptions.path = apiUrl + apiType;
+
+    // create request
+    req = https.request(this.reqOptions, function (res) {
+      var body = '';
+
+      // handle http error
+      if (res.statusCode !== 200)
+        return callback({
+          code: res.statusCode,
+          message: 'HTTP ERROR: bad status code'
+        });
+
+      // ok
+      else {
+        res.on('data', function (chunk) { body += chunk; });
+        res.on('end', function () { callback(null, JSON.parse(body)); });
+      }
+    });
+
+    // handle request error
+    req.on('error', function (err) {
+      callback({
+        code: -1,
+        message: 'REQUEST ERROR: ' + err.message
+      });
+    });
+
+    // send request body
+    // utf8 is by default
+    req.write(JSON.stringify(queries));
+    req.end();
+  }
+}
+
+// service constructor
+function Dadata (API_KEY, SECRET_KEY) {
+
+  // fail if no keys provided
+  if (!API_KEY || !SECRET_KEY) {
+    console.log('node-dadata: keys are strongly required');
+    return null;
+  }
+
+  // don't fail if someone have forgotten about new
+  else if (!(this instanceof Dadata))
+    return new Dadata(arguments);
+
+  // ok
+  else {
+
+    // set request options
+    this.reqOptions = {
+      method: 'POST',
+      hostname: 'dadata.ru',
+      port: 443,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ' + API_KEY,
+        'X-Secret': SECRET_KEY
+      }
+    };
+
+    // return request sender
+    return reqSend.bind(this);
+  }
+}
+
+// exports
+module.exports = Dadata;
